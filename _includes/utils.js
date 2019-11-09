db03af6c78e44b3986688cad7dd30a3464675223
programemos <santsben@gmail.com>
2019-11-09 11:36:22 -0500

    change click for mouseover :p
    I didn't know what to click haha

--- _includes/utils.js
+++ _includes/utils.js
@@ -5,7 +5,7 @@ console.log(cards);
 for (i = 0; i < cards.length; ++i) {
     let card = cards[i];
     
-    card.addEventListener('click', function (e) {
+    card.addEventListener('mouseover', function (e) {
         console.log(card.classList); 
         if (card.classList.contains('flipped')) {
             card.classList.remove('flipped');
